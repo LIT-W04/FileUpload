d608d5a7a198851b03ab8ce0ff4d5c1b1214fbbf
Avrumi Friedman <info@lakewoodprogramming.com>
2017-06-29 22:34:49 -0400

    Initial commit

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<ImageWithText> images = new DB().GetImages();
+            return View(images);
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase image, string text)
+        {
+            string fileName = Guid.NewGuid() + Path.GetExtension(image.FileName);
+            image.SaveAs(Server.MapPath("~/Images") + "/" + fileName);
+            DB db = new DB();
+            db.Add(text, fileName);
+            return Redirect("/home/index");
+        }
+    }
+
+
+    //Title
+    //Description
+    //Name optional/nullable
+    //Phone Number
+    //ImageFileName
+    //Date Listed
+
+    //On the home page, show all the listings, but show only the title, image and date listed.
+    //Next to each listing, there should be a show details button
+
+    //When the show details button is clicked, the user should be taken to a page that shows
+    //all the information of that listing. If the person viewing the listing is also the one that
+    //listed it, there should be a delete button underneath to delete that listing.
+
+    //There should also be a page to add a listing (you can add a link for this in the top navbar
+    //of the site). On this page, there should be a form where users can submit new listings,
+    //inluding uploading of the image
+}
---
+++ WebApplication3/Models/DB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class ImageWithText
+    {
+        public int Id { get; set; }
+        public string ImageText { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public class DB
+    {
+        public IEnumerable<ImageWithText> GetImages()
+        {
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.ConStr);
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Images";
+            List<ImageWithText> result = new List<ImageWithText>();
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ImageWithText text = new ImageWithText
+                {
+                    Id = (int) reader["Id"],
+                    FileName = (string) reader["FileName"],
+                    ImageText = (string) reader["ImageText"]
+                };
+                result.Add(text);
+            }
+
+            return result;
+        }
+
+        public void Add(string text, string fileName)
+        {
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.ConStr);
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "INSERT INTO Images VALUES (@text, @file)";
+            command.Parameters.AddWithValue("@text", text);
+            command.Parameters.AddWithValue("@file", fileName);
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+    }
+}
